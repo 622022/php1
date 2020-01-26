23616fd7e463844364ef5a03a31e4598fbcea30b
Nb <42965940+622022@users.noreply.github.com>
2020-01-26 15:09:59 +0100

    dfgds

--- apna/credentials.php
+++ apna/credentials.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "77.173.204.121";
+$servername = "localhost";
 $username = "s622022_user";
 $password = "Phppassword101";
-$databasename = "users";
+$databasename = "s622022_db";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $databasename);
 
 // Check connection
 if (!$con) {-+
-  
+
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
