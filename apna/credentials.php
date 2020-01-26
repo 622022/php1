9192897f5511e856931e9548a664513e3cb9990e
Nb <42965940+622022@users.noreply.github.com>
2020-01-26 15:07:22 +0100

    blah

--- apna/credentials.php
+++ apna/credentials.php
@@ -1,19 +1,16 @@
 <?php
+$servername = "77.173.204.121";
+$username = "s622022_user";
+$password = "Phppassword101";
+$databasename = "users";
 
-  // Fill in your own credentials here then rename this file to 'credentials.php'
-     define('DB_HOST', '77.173.204.121');
-     define('DB_USER', 's622022_user');
-     define('DB_DB', 's622022_db');
-     define('DB_PASSWORD', 'Phppassword101');
-     define('DB_PORT','3306');
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $databasename);
 
-     $conn = new mysqli(DB_HOST, DB_USER, DB_DB,DB_PASSWORD);
-
-
-    
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
+// Check connection
+if (!$con) {-+
+  
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
