1019b5ded91fbe641f1ffcb693ecd09323c63a0b
Nb <42965940+622022@users.noreply.github.com>
2020-02-27 19:36:10 +0100

    javascipt index removed

--- index.php
+++ index.php
@@ -4,36 +4,22 @@
         <meta charset="utf-8">
         <title>Login</title>
         <link rel="stylesheet" href="css/main.css" />
-        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
     </head>
     <body>
         <!-- //login form which submits data to controller for login and calls validate form method to validate email using Javascript -->
-        <div id="loginForm" class="form" onsubmit="return validateForm()">
+        <div id="loginForm" class="form">
             <h1>Log In</h1>
-            <form id="loginForm" action="controller/controller.php" method="post" onsubmit="return validateForm()">
+            <form id="loginForm" action="controller/controller.php" method="post" >
                 <input id="log-email" type="text" name="login-email" placeholder="email" required />
                 <input type="password" name="login-password" placeholder="password" required />
-                <input name="login-button" type="submit" value="Login" onclick="validateForm()"/>
+                <input name="login-button" type="submit" value="Login" />
             </form>
             <p>Not registered yet? <a href='src/registration.php'>Register Here</a></p>
             <p>Forgot Password? <a href='src/requestpass.php'>Click here.</a></p>
         </div>
 
-        <script>
-        // //the method to validate email
-            function validateForm()
-            {
-                var email = $("#log-email").val();
-                if ((/(.+)@(.+){2,}\.(.+){2,}/.test(email)) || email=="" || email==null) { } else {
-                alert("Please enter a valid email");
-                return false;
-                }
-                document.getElementById("loginForm").submit();
-                return true;
-
-            }
-        </script>
+        
         
     </body>
 </html>
