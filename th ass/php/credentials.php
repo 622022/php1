97a90518cfe65275fa132d4a422c30ca7b1edfc4
Nb <42965940+622022@users.noreply.github.com>
2020-02-27 21:10:53 +0100

    folder changes

--- th ass/php/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Define database server connection details
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 's625242_user');
-    define('DB_DB', 's625242_db');
-    define('DB_PASSWORD', 'SecurePassword1');
-?>
