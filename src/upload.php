69a3179a90ba3239c914a71f51c1f849b53442aa
Nb <42965940+622022@users.noreply.github.com>
2020-02-28 14:39:57 +0100

    removed check from uplaod.php

--- src/upload.php
+++ src/upload.php
@@ -1,12 +1,10 @@
 <?php
 namespace Verot\Upload;
 session_start();
-require_once("../service/loginservice.php");
-$loginService = loginService::getInstance();
-if ($loginService->checkSession())
-{
-?>
+include('../vendor/autoload.php');
 
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -136,7 +134,4 @@ if ($action == 'multiple') {
             }
         }
         </script>
-<?php } else{
-    Echo("You are not logged in!<a href=\"../index.php\">click here to login again</a>.");
-} 
-?>
+
